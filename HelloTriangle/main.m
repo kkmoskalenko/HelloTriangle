639bfc7c919ed5535191330d8a2e5570e48559ee
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2021-04-04 16:03:00 +0700

    Create a blank window

--- HelloTriangle/main.m
+++ HelloTriangle/main.m
@@ -5,12 +5,35 @@
 //  Created by Konstantin Moskalenko on 04.04.2021.
 //
 
-#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
 
-int main(int argc, const char * argv[]) {
+void setupWindow() {
+    NSRect frame = NSMakeRect(0, 0, 400, 400);
+    
+    NSWindowStyleMask style =
+        NSWindowStyleMaskTitled | NSWindowStyleMaskClosable |
+        NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable;
+    
+    NSWindow *window = [[NSWindow alloc]
+                        initWithContentRect:frame styleMask:style
+                        backing:NSBackingStoreBuffered defer:NO];
+    [window setTitle:@"Hello, Triangle!"];
+    [window makeKeyAndOrderFront:nil];
+    
+    CGFloat screenHeight = window.screen.frame.size.height;
+    NSPoint topLeftPoint = NSMakePoint(40, screenHeight - 68);
+    [window setFrameTopLeftPoint:topLeftPoint];
+}
+
+int main(int argc, const char *argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        [NSApplication sharedApplication];
+        [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+        
+        setupWindow();
+        
+        [NSApp activateIgnoringOtherApps:YES];
+        [NSApp run];
     }
     return 0;
 }
