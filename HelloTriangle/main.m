e0c06735f7ec09426cc2c1cd53c176b4ab0381ae
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2021-04-04 13:33:28 +0700

    Initial commit

---
+++ HelloTriangle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloTriangle
+//
+//  Created by Konstantin Moskalenko on 04.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
