5acee64fd8d1e0d3f72df916f39cb8b8df8c60b7
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2021-04-05 23:49:13 +0700

    Display the rendered triangle in the window

--- HelloTriangle/main.m
+++ HelloTriangle/main.m
@@ -6,6 +6,23 @@
 //
 
 #import <AppKit/AppKit.h>
+#import "Renderer.h"
+
+MTKView *createMTKView() {
+    static Renderer *renderer;
+    renderer = [Renderer alloc];
+    
+    MTKView *view = [MTKView new];
+    view.delegate = renderer;
+    
+    view.device = MTLCreateSystemDefaultDevice();
+    NSCAssert(view.device, @"Metal is not supported on this device");
+    
+    renderer = [renderer initWithMetalKitView:view];
+    NSCAssert(renderer, @"Renderer failed initialization");
+    
+    return view;
+}
 
 void setupWindow() {
     NSRect frame = NSMakeRect(0, 0, 400, 400);
@@ -19,10 +36,16 @@ void setupWindow() {
                         backing:NSBackingStoreBuffered defer:NO];
     [window setTitle:@"Hello, Triangle!"];
     [window makeKeyAndOrderFront:nil];
+    [window setContentView:createMTKView()];
     
     CGFloat screenHeight = window.screen.frame.size.height;
     NSPoint topLeftPoint = NSMakePoint(40, screenHeight - 68);
     [window setFrameTopLeftPoint:topLeftPoint];
+    
+    // Terminate the application when the user closes the window
+    [[NSNotificationCenter defaultCenter]
+     addObserver:NSApp selector:@selector(terminate:)
+     name:NSWindowWillCloseNotification object:window];
 }
 
 int main(int argc, const char *argv[]) {
